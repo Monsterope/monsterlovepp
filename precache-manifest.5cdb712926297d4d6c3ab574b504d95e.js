e8636a9b2bb32a14a70fd3b6f746d000a293b1ad
monsterope <dachawat.fang57@sskru.ac.th>
2019-02-20 20:23:19 +0700

    Updates

---
+++ precache-manifest.5cdb712926297d4d6c3ab574b504d95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsterlovepp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0436973482c33947bae5",
+    "url": "/monsterlovepp/static/js/runtime~main.04369734.js"
+  },
+  {
+    "revision": "58620524167a0f4ce5ea",
+    "url": "/monsterlovepp/static/js/main.58620524.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/monsterlovepp/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "58620524167a0f4ce5ea",
+    "url": "/monsterlovepp/static/css/main.dba73617.chunk.css"
+  },
+  {
+    "revision": "4dc750fc8f0ab6982d67eaf1f129f673",
+    "url": "/monsterlovepp/index.html"
+  }
+];
